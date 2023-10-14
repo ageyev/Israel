af99ed34ff01a114ee55b6b7e65b4445155673e0
ageyev <ageyev@gmail.com>
2023-10-14 21:38:52 +0300

    law of war

---
+++ convert_md.sh
@@ -0,0 +1,6 @@
+# to convert .md files to .pdf, .doc etc. use pandoc https://pandoc.org 
+# to install pandoc on Ubuntu/Debian Linux: 
+sudo apt install pandoc texlive texlive-xetex 
+
+# convert to pdf: 
+pandoc ./doc.md --pdf-engine=xelatex -o ./doc.md.pdf
